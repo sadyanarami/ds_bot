aa0ec9dc391cdad757d5740cad102d05e6ccec2d
sadyanarami <klenovdd@gmail.com>
2024-03-31 22:47:50 +0300

    small fix

--- bot.py
+++ bot.py
@@ -15,14 +15,9 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix=">>", intents=intents)
 
-st_accept = "text/html"
-st_useragent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 12_3_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Safari/605.1.15"
-headers = {"Accept": st_accept, "User-Agent": st_useragent}
-
 
 @bot.event
 async def on_ready():
-
     print(f"Logged in as {bot.user} (ID: {bot.user.id})")
     print("------")
 
@@ -34,6 +29,9 @@ async def stat(ctx, profile_name: str, profile_id: str):
             await ctx.send(
                 "Бля, твою стату было настолько больно считать, что я аж заплакал"
             )
+        st_accept = "text/html"
+        st_useragent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 12_3_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Safari/605.1.15"
+        headers = {"Accept": st_accept, "User-Agent": st_useragent}
         req = requests.get(
             f"https://tracker.gg/valorant/profile/riot/{profile_name}%23{profile_id}/overview",
             headers=headers,
